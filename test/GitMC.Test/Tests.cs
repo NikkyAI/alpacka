8f1396bf33eba09ebfa8f04c25d4023427e8f5e1
copygirl <halfnerd.copygirl@gmail.com>
2017-03-09 18:31:51 +0100

    Add very basic pack config loading and test

--- test/GitMC.Test/Tests.cs
+++ test/GitMC.Test/Tests.cs
@@ -1,14 +1,32 @@
 using System;
+using System.IO;
 using Xunit;
+using GitMC.Lib;
 
 namespace GitMC.Test
 {
     public class Tests
     {
-        [Fact]
-        public void TestName()
+        public Tests()
         {
-            Assert.True(true);
+            // Find the workspace root directory by searching for the "gitmc.sln" file.
+            var dir = Directory.GetCurrentDirectory();
+            while (true) {
+                if (File.Exists(Path.Combine(dir, "gitmc.sln"))) break;
+                try { dir = Directory.GetParent(dir).FullName; }
+                catch { throw new Exception("Workspace root directory not found"); }
+            }
+            // Set current working directory to the "run" directory
+            // in the workspace root, creating it if necessary.
+            var cwd = Path.Combine(dir, "run");
+            Directory.CreateDirectory(cwd);
+            Directory.SetCurrentDirectory(cwd);
+        }
+        
+        [Fact]
+        public void LoadConfig()
+        {
+            ModpackConfig.Load("packconfig.yaml");
         }
     }
 }
