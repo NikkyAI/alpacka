03b2ed8ffec46a7ec72bb00dca9d7c2e7cb28a2b
copygirl <halfnerd.copygirl@gmail.com>
2017-03-13 09:39:42 +0100

    Load old mcmod.info format

--- src/GitMC.Lib/Mods/MCModInfo.cs
+++ src/GitMC.Lib/Mods/MCModInfo.cs
@@ -13,30 +13,24 @@ namespace GitMC.Lib.Mods
         // For information on what each field does, see:
         // https://github.com/MinecraftForge/FML/wiki/FML-mod-information-file
         
-        public int ModListVersion { get; set; }
-        public List<Entry> ModList { get; set; } = new List<Entry>();
-        
-        public class Entry
-        {
-            [JsonProperty("modid")]
-            public string ModID { get; set; }
-            public string Name { get; set; }
-            public string Description { get; set; }
-            public string Version { get; set; }
-            [JsonProperty("mcversion")]
-            public string MinecraftVersion { get; set; }
-            public string URL { get; set; }
-            public string UpdateURL { get; set; }
-            public List<string> AuthorList { get; set; } = new List<string>();
-            public string Credits { get; set; }
-            public string LogoFile { get; set; }
-            public List<string> Screenshots { get; set; } = new List<string>();
-            public string Parent { get; set; }
-            public List<string> RequiredMods { get; set; } = new List<string>();
-            public List<string> Dependencies { get; set; } = new List<string>();
-            public List<string> Dependants { get; set; } = new List<string>();
-            public bool UseDependencyInformation { get; set; }
-        }
+        [JsonProperty("modid")]
+        public string ModID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Version { get; set; }
+        [JsonProperty("mcversion")]
+        public string MinecraftVersion { get; set; }
+        public string URL { get; set; }
+        public string UpdateURL { get; set; }
+        public List<string> AuthorList { get; set; } = new List<string>();
+        public string Credits { get; set; }
+        public string LogoFile { get; set; }
+        public List<string> Screenshots { get; set; } = new List<string>();
+        public string Parent { get; set; }
+        public List<string> RequiredMods { get; set; } = new List<string>();
+        public List<string> Dependencies { get; set; } = new List<string>();
+        public List<string> Dependants { get; set; } = new List<string>();
+        public bool UseDependencyInformation { get; set; }
         
         public static async Task<MCModInfo> Extract(Stream modFileStream)
         {
@@ -44,13 +38,25 @@ namespace GitMC.Lib.Mods
             var entry = zip.GetEntry("mcmod.info");
             if (entry == null) throw new Exception("mcmod.info could not be found in mod archive");
             
-            string modinfo;
+            string modInfo;
             using (var reader = new StreamReader(zip.GetInputStream(entry)))
-                modinfo = await reader.ReadToEndAsync();
+                modInfo = await reader.ReadToEndAsync();
             
             var settings = new JsonSerializerSettings {
                 ContractResolver = new CamelCasePropertyNamesContractResolver() };
-            return JsonConvert.DeserializeObject<MCModInfo>(modinfo, settings);
+            
+            List<MCModInfo> infos = (modInfo[0] == '{')
+                ? JsonConvert.DeserializeObject<InfoList>(modInfo, settings).ModList // New format
+                : JsonConvert.DeserializeObject<List<MCModInfo>>(modInfo, settings); // Old format
+            if (infos == null) throw new Exception("mcmod.info doesn't contain a 'modList' entry");
+            if (infos.Count == 0) throw new Exception("mcmod.info contains 0 mod info entries");
+            return infos[0];
+        }
+        
+        public class InfoList
+        {
+            public int ModListVersion { get; set; }
+            public List<MCModInfo> ModList { get; set; }
         }
     }
 }
