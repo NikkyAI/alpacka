8af33a74cb5bc897b34a8d899c032893e4638c02
copygirl <halfnerd.copygirl@gmail.com>
2017-03-18 18:13:38 +0100

    Add Donations URL to EntryLinks

--- src/GitMC.Lib/Config/EntryLinks.cs
+++ src/GitMC.Lib/Config/EntryLinks.cs
@@ -5,5 +5,6 @@ namespace GitMC.Lib.Config
         public string Website { get; set; }
         public string Source { get; set; }
         public string Issues { get; set; }
+        public string Donations { get; set; }
     }
 }
