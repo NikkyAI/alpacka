7d44f2496adf8b86374f1d8c6a3679972c3dcc5b
copygirl <halfnerd.copygirl@gmail.com>
2017-03-20 16:32:31 +0100

    Work around HttpClient not following redirects

--- src/GitMC.Lib/Net/FileDownloaderURL.cs
+++ src/GitMC.Lib/Net/FileDownloaderURL.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Net;
 using System.Net.Http;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
@@ -8,12 +9,23 @@ namespace GitMC.Lib.Net
 {
     public class FileDownloaderURL : IFileDownloader
     {
+        private static readonly int MAX_REDIRECTS = 8;
         private static HttpClient _client = new HttpClient();
         
         public async Task<DownloadedFile> Download(string url)
         {
             var tempPath = Path.GetTempFileName();
             var response = await _client.GetAsync(url);
+            
+            int redirects = 0;
+            while ((response.StatusCode == HttpStatusCode.Moved) ||
+                   (response.StatusCode == HttpStatusCode.Redirect) ||
+                   (response.StatusCode == HttpStatusCode.TemporaryRedirect)) {
+                if (redirects++ > MAX_REDIRECTS)
+                    throw new HttpRequestException($"Too many redirects for URL '{ url }'");
+                response = await _client.GetAsync(response.Headers.Location);
+            }
+            
             response.EnsureSuccessStatusCode();
             
             // Try using suggested file name or getting the it from the request uri.
@@ -25,7 +37,8 @@ namespace GitMC.Lib.Net
             using (var writeStream = new CryptoStream(File.OpenWrite(tempPath), transform, CryptoStreamMode.Write))
                 await response.Content.CopyToAsync(writeStream);
             
-            var md5  = BitConverter.ToString(transform.Hash).Replace("-", "").ToLowerInvariant();
+            var md5 = BitConverter.ToString(transform.Hash)
+                .Replace("-", "").ToLowerInvariant();
             
             return new DownloadedFile(url, tempPath, fileName, md5);
         }
