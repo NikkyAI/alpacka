f21c23c297f41eba91163ec5753cdf8056ed53f9
copygirl <halfnerd.copygirl@gmail.com>
2017-03-30 02:43:52 +0200

    WIP custom console logger

--- src/GitMC.Lib/Logger.cs
+++ src/GitMC.Lib/Logger.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Extensions.Logging;
 
 namespace GitMC.Lib
@@ -9,5 +10,30 @@ namespace GitMC.Lib
             .AddConsole();
         
         public static ILogger Create<T>() => Factory.CreateLogger<T>();
+    }
+    
+    internal class ConsoleLogger : ILogger
+    {
+        private static readonly string[] _logLevelLookup = { "TRACE", "DEBUG", "INFO", "WARN", "ERROR" };
+        
+        public IDisposable BeginScope<TState>(TState state) =>
+            new NullDisposable();
+        
+        public bool IsEnabled(LogLevel logLevel) =>
+            (logLevel > LogLevel.Information);
+        
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state,
+                                Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (!IsEnabled(logLevel)) return;
+            var message = formatter(state, exception);
+            if (string.IsNullOrEmpty(message) && (exception == null)) return;
+            
+            // FIXME
+            Console.Write($"{ _logLevelLookup[(int)logLevel] }: { message }");
+        }
+        
+        class NullDisposable : IDisposable
+            { public void Dispose() {  } }
     }
 }
