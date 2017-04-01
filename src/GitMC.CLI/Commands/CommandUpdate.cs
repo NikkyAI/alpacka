0d7bac1ed5e829ee79e2f2d1e8bee31b2724b5d4
NikkyAi <root@nikky.moe>
2017-04-01 16:44:45 +0200

    WIP CommandUpdate

---
+++ src/GitMC.CLI/Commands/CommandUpdate.cs
@@ -0,0 +1,203 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.CommandLineUtils;
+using Newtonsoft.Json;
+using GitMC.Lib;
+using GitMC.Lib.Config;
+using GitMC.Lib.Net;
+using GitMC.Lib.Instances;
+using LibGit2Sharp;
+
+namespace GitMC.CLI.Commands
+{
+    public class CommandUpdate : CommandLineApplication
+    {
+        public CommandUpdate()
+        {
+            Name = "update";
+            Description = "Update the current gitMC pack";
+            
+            var argVersion = Argument("[version]",
+                "Version to update to, can be a release version (git tag), or any git commit-ish");
+            
+            var optList = Option("-l | --list",
+                "List all pack versions", CommandOptionType.NoValue);
+            
+            HelpOption("-? | -h | --help");
+            
+            OnExecute(async () => {
+                var directory = /*optDirectory.HasValue()
+                    ? Path.GetFullPath(optDirectory.Value())
+                    :*/ Directory.GetCurrentDirectory();
+                
+                if(optList.HasValue() || argVersion.Value == null) {
+                    return ListVersions(directory);
+                }
+                
+                // TODO: switch branches and stuff
+                
+                return await CommandUpdate.Execute(directory);
+                // // read packbuild.json
+                
+                // ModpackVersion build = await GetBuild(directory);
+                
+                // //TODO: download mods
+                
+                // var name = build.Name; //TODO: clean away spaces and special characters
+                // var prettyName = build.Name;
+                // var mcVersion = build.MinecraftVersion;
+                // var forgeVersion = build.ForgeVersion;
+                
+                // var forgeData = await ForgeVersionData.Download();
+                // ForgeVersion forge = forgeData[forgeVersion];
+                // var info = GitMCInfo.Load(directory);
+                
+                // if(info.Type == InstallType.Server)
+                // {
+                //     var forgeFile = await ForgeInstaller.InstallServer(directory, build, forge);
+                    
+                //     Console.WriteLine($"start forge server by executing {forgeFile}");
+                    
+                //     var modsDir = Path.Combine(directory, Constants.MC_MODS_DIR);
+                    
+                //     await DownloadMods(build.Mods, modsDir);
+                    
+                //     // TODO: mabye later use ModpackDownloader
+                //     // List<DownloadedMod> downloaded;
+                //     // using (var modsCache = new FileCache(Path.Combine(Constants.CachePath, "mods")))
+                //     // using (var downloader = new ModpackDownloader(modsCache)
+                //     //         .WithSourceHandler(new ModSourceURL()))
+                //     //     downloaded = await downloader.Run(modpackVersion);
+                        
+                //     // foreach (var downloadedMod in downloaded.Where(d => d.Mod.Side.IsServer()))
+                //     //     File.Copy(downloadedMod.File.Path, Path.Combine(modsDir, downloadedMod.File.FileName));
+                //     return 0;
+                // }
+                
+                // return 0;
+            });
+        }
+        
+        public static int ListVersions(string directory, bool withLocal = false) {
+            using (var repo = new Repository(directory))
+            {
+                var tip = repo.Head.Tip;
+                Console.WriteLine($"Tip: { repo.Head.FriendlyName }{ tip }");
+                Console.WriteLine("Branches:");
+                foreach(Branch b in repo.Branches.Where(b => b.IsRemote || b.IsTracking))
+                {
+                    var prefix = b.IsCurrentRepositoryHead ? "*" : " ";
+                    var remote = b.IsRemote ? "remote" : "";
+                    var tracking = b.IsTracking ? "tracking" : "";
+                    Console.WriteLine($"{ prefix }{ b.FriendlyName } { remote }{ tracking }");
+                    // Console.WriteLine(string.Format("{0}{1}", b.IsCurrentRepositoryHead ? "*" : " ", b.FriendlyName));
+                }
+                Console.WriteLine("Tags:");
+                foreach (Tag t in repo.Tags.Reverse())
+                {
+                    var target = (Commit) t.Target;
+                    
+                    var prefix = target == tip ? "*" : " ";
+                    var name = t.FriendlyName[0] == 'v' ? t.FriendlyName.Substring(1) : t.FriendlyName;
+                    Console.WriteLine($"{ prefix }{ name }");
+                }
+            }
+            
+            return 0;
+        }
+        
+        //TODO: rename to Install or similar
+        public static async Task<int> Execute(string directory, ModpackBuild build = null)
+        {
+            if (build == null) build = await GetBuild(directory);
+            
+            var name       = string.Join("_", build.Name.Split(Path.GetInvalidPathChars()));
+            var prettyName = build.Name;
+            var mcVersion  = build.MinecraftVersion;
+            
+            var forgeData    = await ForgeVersionData.Download();
+            var forgeVersion = forgeData[build.ForgeVersion];
+            
+            var modsDir   = Path.Combine(directory, Constants.MC_MODS_DIR);
+            var gitMCInfo = GitMCInfo.Load(directory);
+            switch (gitMCInfo.Type) {
+                
+                case InstallType.MultiMC:
+                    var packInstanceFolder = Directory.GetParent(directory).FullName;
+                    var instanceConfigPath = Path.Combine(packInstanceFolder, "instance.cfg");
+                    if (!File.Exists(instanceConfigPath))
+                        throw new Exception("Not a MultiMC instance");
+                    
+                    // update minecraft version in instance.cfg
+                    var instanceCfg = File.ReadAllText(instanceConfigPath);
+                    var intendedVersion = $"\nIntendedVersion={ mcVersion }";
+                    if (!instanceCfg.Contains(intendedVersion)) {
+                        instanceCfg += intendedVersion;
+                        File.WriteAllText(instanceConfigPath, instanceCfg);
+                    }
+                    
+                    // TODO: copy icon and set icon = gitm_{ name }
+                    
+                    Console.WriteLine($"Installing Forge { build.ForgeVersion } ...");
+                    var installedForge = await ForgeInstaller.InstallMultiMC(packInstanceFolder, build);
+                    
+                    Console.WriteLine("Downloading mods ...");
+                    await DownloadMods(build.Mods, Side.Client, modsDir);
+                    break;
+                
+                case InstallType.Server:
+                    Console.WriteLine("Downloading mods ...");
+                    await DownloadMods(build.Mods, Side.Server, modsDir);
+                    
+                    Console.WriteLine($"Installing Forge { build.ForgeVersion } ...");
+                    var forgeFile = await ForgeInstaller.InstallServer(directory, build, forgeVersion);
+                    
+                    Console.WriteLine($"Start forge server by executing { forgeFile }");
+                    
+                    // TODO: mabye later use ModpackDownloader
+                    // List<DownloadedMod> downloaded;
+                    // using (var modsCache = new FileCache(Path.Combine(Constants.CachePath, "mods")))
+                    // using (var downloader = new ModpackDownloader(modsCache)
+                    //         .WithSourceHandler(new ModSourceURL()))
+                    //     downloaded = await downloader.Run(modpackVersion);
+                        
+                    // foreach (var downloadedMod in downloaded.Where(d => d.Mod.Side.IsServer()))
+                    //     File.Copy(downloadedMod.File.Path, Path.Combine(modsDir, downloadedMod.File.FileName));
+                    break;
+                
+                default:
+                    throw new NotImplementedException();
+                
+            }
+            
+            return 0;
+        }
+        
+        public static async Task<ModpackBuild> GetBuild(string directory)
+        {
+            var packBuildPath = Path.Combine(directory, Constants.PACK_BUILD_FILE);
+            return File.Exists(packBuildPath)
+                ? JsonConvert.DeserializeObject<ModpackBuild>(File.ReadAllText(packBuildPath))
+                : await CommandBuild.Build(ModpackConfig.LoadYAML(directory));
+        }
+        
+        public static Task DownloadMods(List<EntryMod> modList, Side side, string modsDir) {
+            if (Directory.Exists(modsDir))
+                Directory.Delete(modsDir, true);
+            Directory.CreateDirectory(modsDir);
+            
+            var mods = modList.Where(mod => (mod.Side & side) == side).ToList(); 
+            using (var fileCache = new FileCache(Path.Combine(Constants.CachePath, "mods")))
+            using (var downloader = new FileDownloaderURL(fileCache))
+                return Task.WhenAll(mods.Select(async mod => {
+                    var file = await downloader.Download(mod.Source);
+                    if ((mod.MD5 != null) && (mod.MD5 != file.MD5))
+                        throw new Exception($"MD5: '{ mod.MD5 }' does not match downloaded file's MD5: '{ file.MD5 }' { mod.Name }");
+                    File.Copy(file.Path, Path.Combine(modsDir, file.FileName), true);
+                }));
+        }
+    }
+}
---
+++ src/GitMC.Lib/Git/UpdateUtil.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using LibGit2Sharp;
+
+namespace GitMC.Lib.Git
+{
+    public static class UpdateUtil
+    {
+        public static string Checkout(string workingDirectory, string branch)
+        {
+            // var directory = Path.Combine(workingDirectory, $"{ Guid.NewGuid() }");
+            // Repository.Clone(url, directory);
+            
+            // return branch;
+            return "";
+        }
+    }
+}
