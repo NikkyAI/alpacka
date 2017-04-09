3290605797405bb1cc2d711596e22b0103272abf
NikkyAi <root@nikky.moe>
2017-04-10 00:42:29 +0200

    changed release procedure
    - check for changed files
    - calculate version
    - checkout tip of release
    - merge commit from master into release
    - add packbuild and commit to release
    - add tag
    - checkout master again

--- src/GitMC.CLI/Commands/CommandRelease.cs
+++ src/GitMC.CLI/Commands/CommandRelease.cs
@@ -23,13 +23,13 @@ namespace GitMC.CLI.Commands
             Description = "Releases the current gitMC pack";
             
             var argVersion = Argument("[version]",
-                "Version that is released defaults to current.");
+                "Version that is released, defaults to current. Cannot be used with -i | --increase");
                 
             var optIncrease = Option("-i | --increase",
-                "Increase the pack version [patch, minor, major]", CommandOptionType.SingleValue);
+                "Increase the pack version [patch, minor, major] default: patch. Cannot be used with [version]", CommandOptionType.SingleValue);
             
             var optPush = Option("-p | --push",
-                "automatically push the release", CommandOptionType.NoValue);
+                "automatically push the release. Pushes 'release' branch and tag to 'origin', also sets upstream for 'release' branch. Does NOT push other tags", CommandOptionType.NoValue);
                 
             HelpOption("-? | -h | --help");
             
@@ -38,14 +38,37 @@ namespace GitMC.CLI.Commands
                 var directory = Directory.GetCurrentDirectory();
                 
                 if (!string.IsNullOrEmpty(argVersion.Value) && optIncrease.HasValue()) {
-                    Console.WriteLine("ERROR: version number and --increasde set at the same time");
+                    Console.WriteLine("ERROR: version number and --increase set at the same time");
+                    ShowHelp();
                     return 1;
                 }
                 
-                var packConfig = ModpackConfig.LoadYAML(directory);
-                var build  = await Build(packConfig);
+                if (string.IsNullOrEmpty(argVersion.Value) && !optIncrease.HasValue()) {
+                    Console.WriteLine("ERROR: set either version number or --increase");
+                    ShowHelp();
+                    return 0;
+                }
+                
+                // var packConfig = ModpackConfig.LoadYAML(directory);
+                // var build  = await Build(packConfig);
                 
                 using (var repo = new Repository(directory)) {
+                    // check if we are on master
+                    if (repo.Head.FriendlyName != "master") {
+                        Console.WriteLine("ERROR: Not on working branch, you cannot make a release like this");
+                        return 1;
+                    }
+                    
+                    // check for changed files
+                    if ((repo.RetrieveStatus()).Count() != 0) {
+                        Console.WriteLine("ERROR: commit, stash or discard changes:");
+                        foreach (var f in repo.RetrieveStatus())
+                        {
+                            Console.WriteLine($"> { f.FilePath }");
+                        }
+                        return 1;
+                    }
+                    
                     // get highest version
                     var allTagVersions = repo.Tags.Select(t => {
                         var vString = t.FriendlyName;
@@ -64,8 +87,11 @@ namespace GitMC.CLI.Commands
                     var versionString = argVersion.Value;
                     
                     System.Version version = null;
-                    if (!string.IsNullOrEmpty(versionString) && System.Version.TryParse(versionString, out version)) {
-                        
+                    if(!string.IsNullOrEmpty(versionString)) {
+                        if (!System.Version.TryParse(versionString, out version)) {
+                            Console.WriteLine($"failed to parse '{ versionString }' as version");
+                            return 1;
+                        }
                     } else {
                         if (optIncrease.HasValue()) {
                             SemanticVersion increaseVersion;
@@ -91,58 +117,103 @@ namespace GitMC.CLI.Commands
                                     break;
                                 default:
                                     Console.WriteLine("ERROR unexpected SemanticVersion");
-                                    break;
+                                    return 1;
                             }
                             version = new System.Version(major, minor, patch);
+                        } else {
+                            
+                            return 1;
                         }
                     }
                     
-                    //set pack version
-                    build.PackVersion = version?.ToString() ?? versionString;
-                    
-                    if (allTagVersions.Any( a => (a.Version?.ToString() ?? "") == build.PackVersion )) {
-                        Console.WriteLine("ERROR version tag already exists");
+                    if(version < lastVersion) {
+                        //TODO: add flag to ignore this warning
+                        Console.WriteLine($"WARNING: version {version} is smaller than highest version {lastVersion}");
                         return 1;
                     }
                     
-                    Console.WriteLine($"selected version { build.PackVersion }");
+                    var buildVersion = version.ToString();
+                    var tagName = $"v{buildVersion}";
                     
-                    build.SaveJSON(directory, pretty: true);
+                    if (allTagVersions.Any( a => (a.Version?.ToString() ?? "") == buildVersion )) {
+                        Console.WriteLine($"ERROR: version tag 'v{buildVersion}' already exists");
+                        return 1;
+                    }
                     
-                    // Stage the build file
-                    LibGit2Sharp.Commands.Stage(repo, Constants.PACK_BUILD_FILE, new StageOptions{IncludeIgnored = true});
+                    Console.WriteLine($"selected version { buildVersion }");
                     
-                    Commit parentCommit;
-                    using( var config = Configuration.BuildFrom(".")) {
+                    var packConfig = ModpackConfig.LoadYAML(directory);
+                    var build  = await Build(packConfig);
+                    
+                    //set pack version
+                    build.PackVersion = buildVersion; // version?.ToString() ?? versionString;
+                    
+                    
+                    Commit masterCommit = repo.Head.Tip;
+                    Branch releaseBranch = repo.Branches["release"];
+                    
+                    // check if branch releases exists
+                    if(releaseBranch == null) {
+                        Console.WriteLine("creating branch 'release'");
+                        // create release branch at current commit
+                        releaseBranch = repo.CreateBranch("release", masterCommit);
+                        //TODO: set-upstream origin/release
+                    }
+                    
+                    // checkout release
+                    LibGit2Sharp.Commands.Checkout(repo, releaseBranch, new CheckoutOptions{ CheckoutModifiers = CheckoutModifiers.Force });
+                    // reset to tip of release
+                    repo.Reset(ResetMode.Hard, releaseBranch.Tip);
+                    
+                    using( var config = Configuration.BuildFrom(".") ) {
                         // Create the committer's signature and commit
                         //TODO: ask for and set git config if not set
                         var name = config.GetValueOrDefault<string>("user.name", "nobody");
                         var email = config.GetValueOrDefault<string>("user.email", "@example.com");
-                        Signature author = new Signature(name, email, DateTime.Now);
-                        Signature committer = author;
+                        Signature user = new Signature(name, email, DateTime.Now);
+                        
+                        // merge master into release
+                        var mergeResult = repo.Merge(masterCommit, user, new MergeOptions{ FastForwardStrategy = FastForwardStrategy.Default, FileConflictStrategy = CheckoutFileConflictStrategy.Theirs });
+                        
+                        // rewrite commit message
+                        if(mergeResult.Status == MergeStatus.NonFastForward && mergeResult.Commit != null) {
+                            // remove old backup refs
+                            repo.Refs.Remove($"refs/original/heads/{ releaseBranch.FriendlyName }");
+                            repo.Refs.RewriteHistory (new RewriteHistoryOptions {
+                                CommitHeaderRewriter = c =>
+                                    CommitRewriteInfo.From (c, $"preparing { build.PackVersion } \n{ masterCommit.Message }"),
+                            }, mergeResult.Commit);
+                        }
+                        
+                        build.SaveJSON(directory, pretty: true);
+                        
+                        // Stage the build file
+                        LibGit2Sharp.Commands.Stage(repo, Constants.PACK_BUILD_FILE, new StageOptions{IncludeIgnored = true});
+                        
                         // Commit to the repository
-                        Commit commit = repo.Commit($"Release { build.PackVersion }", author, committer);
-                        parentCommit = commit.Parents.First();
-                    }
-                    var tagName = $"v{version}";
-                    // create tag
-                    {
-                        Tag t = repo.ApplyTag(tagName);
+                        Commit commit = repo.Commit($"Release { build.PackVersion }", user, user);
                     }
                     
+                    // create tag
+                    Tag tag = repo.ApplyTag(tagName);
+                    
                     // push to remote
+                    //TODO: get username and password or make key based auth work
                     if (optPush.HasValue()) {
                         foreach(var r in repo.Network.Remotes) {
                             Console.WriteLine($"r.Name");
                             var remote = r.Name;
-                            var ret = await ThreadUtil.RunProcessAsync("git", $"push { remote } { tagName }");
-                            Console.WriteLine($"git push { remote } { tagName } finished with exit code { ret }");
+                            var retTag = await ThreadUtil.RunProcessAsync("git", $"push { remote } { tagName }");
+                            Console.WriteLine($"git push { remote } { tagName } finished with exit code { retTag }");
+                            var retBranch = await ThreadUtil.RunProcessAsync("git", $"push { remote } { releaseBranch.FriendlyName }");
+                            Console.WriteLine($"git push { remote } { releaseBranch.FriendlyName } finished with exit code { retBranch }");
                         }
+                    } else {
+                        Console.WriteLine($"Don't forget to push branch '{ releaseBranch.FriendlyName }' and tag '{ tag.FriendlyName }'");
                     }
                     
-                    repo.Reset(ResetMode.Hard, parentCommit);
+                    LibGit2Sharp.Commands.Checkout(repo, "master");//, new CheckoutOptions{ CheckoutModifiers = CheckoutModifiers.Force });
                 }
-                
                 return 0;
             });
         }
