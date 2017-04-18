fbfef17902be3b622b279cde92c7559e17594489
copygirl <halfnerd.copygirl@gmail.com>
2017-04-18 19:32:50 +0200

    Add IInstanceHandler

---
+++ src/Alpacka.Lib/Instances/IInstanceHandler.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using Alpacka.Lib.Config;
+
+namespace Alpacka.Lib.Instances
+{
+    public interface IInstanceHandler
+    {
+        /// <summary> Gets a unique name for this instance type. </summary>
+        string Name { get; }
+        
+        /// <summary> Gets the full path of an instance for this type with
+        ///           the specified simplified (safe) instance / folder name. </summary>
+        string GetInstancePath(string instanceName);
+        
+        /// <summary> Returns the list of instance paths that are installed
+        ///           for this instance type. Returns null if not supported. </summary>
+        List<string> GetInstances();
+        
+        
+        /// <summary>
+        ///   Installs a new instance in the specified path
+        ///   for this type using the specified modpack data.
+        ///   
+        ///   Handles Forge and instance metadata, not installing mods / configs.
+        /// </summary>
+        void Install(string instancePath, ModpackBuild pack);
+        
+        /// <summary>
+        ///   Updates the instance in the specified path.
+        ///   
+        ///   Handles Forge and instance metadata, not updating mods / configs.
+        /// </summary>
+        // FIXME: Currently, oldPack isn't required at the moment but would be useful.
+        void Update(string instancePath, ModpackBuild oldPack, ModpackBuild newPack);
+        
+        /// <summary>
+        ///   Removes the instance in the specified path.
+        /// </summary>
+        void Remove(string instancePath);
+    }
+}
